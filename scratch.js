b41bef405ecbfeff0454098ba9a7a19a8bfe125b
Kevin Nguyen <kevinnguyen@Kevins-MacBook-Air.local>
2019-03-28 18:27:52 -0700

    Added BinarySearch

--- scratch.js
+++ scratch.js
@@ -1,31 +1,4 @@
-function anagrams(stringA, stringB) {
-  const charCountObjA = stringA
-    .replace(/[^A-Za-z0-9]/g, "")
-    .toLowerCase()
-    .split("")
-    .reduce((obj, char) => {
-      obj[char] = obj[char] + 1 || 1;
-      return obj;
-    }, {});
+//Searching for number 30
 
-  const charCountObjB = stringB
-    .replace(/[^A-Za-z0-9]/g, "")
-    .toLowerCase()
-    .split("")
-    .reduce((obj, char) => {
-      obj[char] = obj[char] + 1 || 1;
-      return obj;
-    }, {});
-
-  if (Object.keys(charCountObjA).length !== Object.keys(charCountObjB).length) {
-    return false;
-  }
-
-  for (key in charCountObjA) {
-    if (charCountObjA[key] !== charCountObjB[key]) {
-      return false;
-    }
-  }
-
-  return true;
-}
+// [5, 10, 15, 20, 25, 30, 35, 40]
+//  LP         MP              RP
