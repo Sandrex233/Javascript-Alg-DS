418b5736270439d5bc00ad144c6bce5d723caf7f
kevin nguyen <kevinrsdnguyen@gmail.com>
2019-04-02 19:34:12 -0700

    Updated sorting

--- sorting/index.js
+++ sorting/index.js
@@ -1,5 +1,5 @@
-// --- Directions
-// Implement bubbleSort and selectionSort, with bigger numbers towards end
+// Implement a swap helper function that we will use in both BS and SS
+function swap(arr, i, j) {}
 
 //bubbleSort works by having sorted data accumulate at end of array
 function bubbleSort(arr) {}
@@ -33,13 +33,21 @@ function selectionSort(arr) {}
 mocha.setup("bdd");
 const { assert } = chai;
 
-describe("Bubble Sort", () => {
-  it("sorts an array", () => {
-    assert.deepEqual(bubbleSort([5, 1, 3, 2, 4]), [1, 2, 3, 4, 5]);
+describe("swap()", () => {
+  it("swaps values in an array when provided with 2 indexes.", () => {
+    const arr = [13, 2, 4];
+    swap(arr, 0, 1);
+    assert.deepEqual(arr, [2, 13, 4]);
   });
 });
 
-describe("Selection Sort", () => {
+describe.skip("Bubble Sort", () => {
+  it("sorts an array", () => {
+    assert.deepEqual(bubbleSort([5, 1, 3, 7, 6, 2, 4]), [1, 2, 3, 4, 5, 6, 7]);
+  });
+});
+
+describe.skip("Selection Sort", () => {
   it("sorts an array", () => {
     assert.deepEqual(selectionSort([5, 1, 3, 2, 4]), [1, 2, 3, 4, 5]);
   });
