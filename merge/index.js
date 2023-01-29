101e86c50462e8cafcfc02cabf3839fa17596c04
Sandrex233 <sandrex233@gmail.com>
2023-01-29 21:45:25 +0400

    merge - O(n) time

--- merge/index.js
+++ merge/index.js
@@ -10,7 +10,35 @@ function merge(sortedArr1, sortedArr2) {
   /* return sortedArr1.concat(sortedArr2).sort((a, b) => a - b) */
 
   // Method 2
-  return [...sortedArr1, ...sortedArr2].sort((a, b) => a - b)
+  // return [...sortedArr1, ...sortedArr2].sort((a, b) => a - b)
+
+  // Correct Method - O(n)
+  let p1 = 0
+  let p2 = 0
+  let res = []
+
+  while (p1 < sortedArr1.length && p2 < sortedArr2.length) {
+    if (sortedArr1[p1] < sortedArr2[p2]) {
+      res.push(sortedArr1[p1])
+      p1++
+    } else {
+      res.push(sortedArr2[p2])
+      p2++
+    }
+  }
+
+  while (p1 < sortedArr1.length) {
+    res.push(sortedArr1[p1])
+    p1++
+  }
+
+
+  while (p2 < sortedArr2.length) {
+    res.push(sortedArr2[p2])
+    p2++
+  }
+
+  return res
 }
 
 
