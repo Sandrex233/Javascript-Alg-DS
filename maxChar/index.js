cbb6d2e7280bfb9dc77da6b97f57dcb91d9c591c
Sandrex233 <sandrex233@gmail.com>
2023-02-06 18:38:52 +0400

    maxChar

--- maxChar/index.js
+++ maxChar/index.js
@@ -3,7 +3,22 @@
 // maxChar("I loveeeeeee noodles") === "e"
 // maxChar("1337") === "3"
 
-function maxChar(str) {}
+function maxChar(str) {
+  let countObj = {}
+  let maxChar = ""
+  let maxCount = 0
+
+  for (let i = 0; i < str.length; i++) {
+    countObj[str[i]] = countObj[str[i]] + 1 || 1
+
+    if (countObj[str[i]] > maxCount) {
+      maxChar = str[i]
+      maxCount = countObj[str[i]]
+    }
+  }
+
+  return maxChar
+}
 
 // _________ _______  _______ _________   _______  _______  _______  _______  _______
 // \__   __/(  ____ \(  ____ \\__   __/  (  ____ \(  ___  )(  ____ \(  ____ \(  ____ \
