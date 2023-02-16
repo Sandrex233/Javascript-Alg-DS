61fc565ac02551ec17996f6fe229171d45d2125b
Sandrex233 <sandrex233@gmail.com>
2023-02-17 00:14:55 +0400

    linkedlist shift

--- linked-list/index.js
+++ linked-list/index.js
@@ -37,6 +37,17 @@ class LinkedList {
 		this.head = null;
 		this.length = 0;
 	}
+
+	shift() {
+		if (!this.head) {
+			return
+		}
+
+		const oldHead = this.head
+		this.head = this.head.next
+		this.length--
+		return oldHead
+	}
 }
 
 // _________ _______  _______ _________   _______  _______  _______  _______  _______
@@ -125,7 +136,7 @@ describe('clear()', () => {
 	});
 });
 
-describe.skip('shift()', () => {
+describe('shift()', () => {
 	it('removes AND returns first node, updates length for linked list w/ one node.', () => {
 		const l = new LinkedList();
 		l.unshift(1);
